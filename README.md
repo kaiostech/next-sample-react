e04def4feb4e78272e110cb9554681fce95f402c
xin.jin <xin.jin@kaiostech.com>
2020-12-23 18:46:07 -0800

    repo init

---
+++ README.md
@@ -0,0 +1,35 @@
+# Sample React app for KaiOS
+
+Simple example of a to-do list, for more information see [KaiOS Developer Portal](https://developer.kaiostech.com/getting-started/build-your-first-app/sample-code#react)
+
+![](./docs/to-do-on-input.png)
+![](./docs/to-do.png)
+
+In portrait devices
+
+![](./docs/to-do-portrait.gif)
+
+In landscape devices
+
+![](./docs/to-do-landscape.gif)
+
+## Start
+
+```console
+npm run start
+# or
+yarn start
+```
+
+## Build app
+
+```console
+npm run build
+# or
+yarn build
+```
+
+## Send the app to a KaiOS device
+
+follow [os-env-setup](https://developer.kaiostech.com/getting-started/env-setup/os-env-setup) and [test-your-apps](https://developer.kaiostech.com/getting-started/build-your-first-package-app/test-your-apps)
+install to your device.
---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Header, Input, ToDos, Softkey } from "./components";
+import { useNavigation } from "./hooks";
+
+export default function App() {
+  const [toDos, setToDo] = useState([]);
+
+  const [current, setNavigation] = useNavigation();
+
+  const onKeyCenter = () => {
+    const currentElement = document.querySelector("[nav-selected=true]");
+    const currentNavigationIndex = parseInt(currentElement.getAttribute("nav-index"), 10);
+
+    const isATask = currentNavigationIndex > 0;
+    if (isATask) {
+      setToDo(prevState => {
+        const current = [...prevState];
+        current[currentNavigationIndex - 1].completed = !current[currentNavigationIndex - 1].completed;
+        return current;
+      });
+    } else if (currentElement.value.length) {
+      const toDo = { name: currentElement.value, completed: false };
+      setToDo(prevState => [...prevState, toDo]);
+      currentElement.value = "";
+    }
+  };
+
+  const onKeyRight = () => {
+    const currentIndex = parseInt(
+      document.querySelector("[nav-selected=true]").getAttribute("nav-index"),
+      10
+    );
+    if (currentIndex > 0) {
+      setToDo(prevState => {
+        const current = [...prevState];
+        current.splice(currentIndex - 1, 1);
+        const goToPreviousElement = Boolean(current.length);
+        setNavigation(goToPreviousElement ? currentIndex - 1 : 0);
+        return current;
+      });
+    }
+  };
+
+  return (
+    <>
+      <Header title="ToDo List" />
+
+      <Input type="text" label="New task" />
+      <ToDos toDos={toDos} />
+
+      <Softkey
+        center={current.type === "INPUT" ? "Insert" : "Toggle"}
+        onKeyCenter={onKeyCenter}
+        right={current.type === "SPAN" ? "Delete" : ""}
+        onKeyRight={onKeyRight}
+      />
+    </>
+  );
+}
